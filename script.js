06b4fb5114071fbc1a3c0838c9fffa1fc22173a7
mariperz438-web <mariperz438@gmail.com>
2026-03-31 17:28:16 -0400

    ultimas modificaciones

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const formulario = document.querySelector("#formulario-moto");
 
 formulario.addEventListener("submit", (e) => {
-  e.preventDefault(); // Evita que la página se recargue
+  e.preventDefault(); 
 
-  // 1. Capturamos los datos
+
   const nombre = document.querySelector("#nombre").value;
   const producto = document.querySelector("#producto").value;
-  const telefono = "584245409099"; // PONE TU NÚMERO AQUÍ (con código de país)
+  const mensaje = document.querySelector("#mensaje").value;
+  const telefono = "584245409099"; 
 
-  // 2. Armamos el mensaje para WhatsApp
-  const mensajeWhatsApp = `Hola Mari! Mi nombre es ${nombre}. Me gustaría contratar el servicio de: ${producto}.`;
 
-  // 3. Creamos la URL mágica de WhatsApp
+  const mensajeWhatsApp = `Hola Mari! Mi nombre es ${nombre}. Me gustaría contratar el servicio de: ${producto}. ${mensaje}`;
+
+
   const url = `https://api.whatsapp.com/send?phone=${telefono}&text=${encodeURIComponent(mensajeWhatsApp)}`;
 
-  // 4. ¡Mandamos al usuario al chat!
+
   window.open(url, "_blank");
 });
